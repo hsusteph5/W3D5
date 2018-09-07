b9706166021f9f93e6ec4f16d8d172580e9e6cdc
Stephanie Hsu <hsusteph5@gmail.com>
2018-09-07 16:55:41 -0700

    Add setters and getter methods for each col

--- ActiveRecordLite/lib/01_sql_object.rb
+++ ActiveRecordLite/lib/01_sql_object.rb
@@ -1,22 +1,45 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
-    # ...
+    table = self.table_name
+    @arr ||= DBConnection.execute2(<<-SQL)
+    SELECT
+      *
+    FROM
+      #{table}
+    SQL
+    @arr.first.map {|el| el.to_sym}
   end
 
   def self.finalize!
+    # debugger
+    cols = self.columns
+    cols.each do |name|
+      define_method(name) do
+        attributes[name]
+      end
+
+#attributes the method[name]
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+
   end
 
   def self.table_name
-    # ...
+    # debugger
+    name = "#{self.to_s.downcase}s"
   end
 
   def self.all
@@ -32,11 +55,14 @@ class SQLObject
   end
 
   def initialize(params = {})
+    raise 
     # ...
   end
 
   def attributes
-    # ...
+    #want attributes to be hash
+    @attributes ||= {}
+
   end
 
   def attribute_values
